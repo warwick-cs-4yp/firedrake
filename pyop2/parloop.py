9768346d46985765be873b1a7b049aa22112dc36
LeilaDavis <98031884+LeilaDavis@users.noreply.github.com>
2026-04-06 17:36:29 +0100

    Removed erroneous import statement

--- pyop2/parloop.py
+++ pyop2/parloop.py
@@ -19,7 +19,6 @@ from pyop2.local_kernel import LocalKernel, CStringLocalKernel, LoopyLocalKernel
 from pyop2.types import (Access, Global, AbstractDat, Dat, DatView, MixedDat, Mat, Set,
                          MixedSet, ExtrudedSet, Subset, Map, ComposedMap, MixedMap)
 from pyop2.types.data_carrier import DataCarrier
-from pyop2.utils import cached_property
 from pyop2.types import (READ, WRITE, RW, INC, MIN, MAX)
 
 import umesh_jit as umesh
